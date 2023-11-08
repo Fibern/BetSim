0fd0b050630a58c6736d1b7daa425845d98e8bf6
Wojtek Brozek <wobrozek@gmail.com>
2023-11-08 15:04:23 +0100

    add dbContext and configure ef core

---
+++ backend/BetSimApi/DbMainContext.cs
@@ -0,0 +1,19 @@
+﻿using BetSimApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BetSimApi
+{
+    public class DbMainContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+        public DbSet<User> User { get; set; }
+        public DbSet<Bet> Bet { get; set; }
+        public DbSet<Odd> Odd { get; set; }
+        public DbSet<User> Event { get; set; }
+    }
+}
--- backend/BetSimApi/Program.cs
+++ backend/BetSimApi/Program.cs
@@ -1,6 +1,11 @@
+using BetSimApi;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DbMainContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("SqlServer"),
+    builder => builder.EnableRetryOnFailure()));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
