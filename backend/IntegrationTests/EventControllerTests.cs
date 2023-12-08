9d5563d1f97f791b0d67905da2dbd782d703c3bf
Wojtek Brozek <wobrozek@gmail.com>
2023-12-08 22:27:00 +0100

    fixture test data

--- backend/IntegrationTests/EventControllerTests.cs
+++ backend/IntegrationTests/EventControllerTests.cs
@@ -3,21 +3,25 @@ using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace IntegrationTests
 {
-    public class EventControllerTests
+    public class EventControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
+   
+        private readonly HttpClient _client;
+
+        internal EventControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
         //arrange
         [Fact]
         public async Task GetAll_returnOk()
         {
-            //arrange
-
-            var factory = new WebApplicationFactory<Program>();
-
-            var client = factory.CreateClient();
-
             //act 
 
-            var response = await client.GetAsync("/Event");
+            var response = await _client.GetAsync("/Event");
+
+            //assertion
 
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
 
