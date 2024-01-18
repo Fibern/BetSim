0f384ac05579447777c20dcc699143215e7c5ea3
fibern <domagala.john@gmail.com>
2024-01-19 00:47:10 +0100

    Login Screen readiness for Api integration

--- frontend/app/src/main/java/com/example/betsim/presentation/auth/LoginScreen.kt
+++ frontend/app/src/main/java/com/example/betsim/presentation/auth/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.betsim.presentation.auth
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,19 +14,22 @@ import androidx.compose.material.icons.rounded.Lock
 import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.betsim.presentation.util.Screen
 import com.example.betsim.presentation.auth.components.AuthClickableText
-import com.example.betsim.presentation.auth.components.AuthTextField
 import com.example.betsim.presentation.auth.components.AuthText
+import com.example.betsim.presentation.auth.components.AuthTextField
 import com.example.betsim.presentation.common.components.BetSimButton
+import com.example.betsim.presentation.common.components.SemiTransparentLoadingScreen
+import com.example.betsim.presentation.util.Screen
 
 @Composable
 fun LoginScreen(
@@ -34,6 +38,31 @@ fun LoginScreen(
 ) {
     val login by remember { viewModel.login }
     val password by remember { viewModel.password }
+    val toast by remember { viewModel.toastMessage }
+    val isLoading by remember { viewModel.isLoading }
+    val success by remember { viewModel.success }
+    val context = LocalContext.current
+
+    if (toast.isNotBlank()) {
+        LaunchedEffect(toast) {
+            Toast.makeText(
+                context,
+                toast,
+                Toast.LENGTH_SHORT
+            ).show()
+            viewModel.clearToast()
+        }
+    }
+
+    LaunchedEffect(success) {
+        if (success) {
+            navController.navigate(Screen.UserNav.route){
+                popUpTo(Screen.AuthNav.route){
+                    inclusive = false
+                }
+            }
+        }
+    }
 
     Surface(
         modifier = Modifier
@@ -50,21 +79,23 @@ fun LoginScreen(
             Spacer(modifier = Modifier.height(10.dp))
 
             AuthTextField(
-                text = login,
+                text = login.value,
                 label = "Login",
                 onValChange = { viewModel.onEvent(AuthEvent.EnteredLogin(it)) },
-                icon = Icons.Rounded.Person
+                icon = Icons.Rounded.Person,
+                isError = login.isError,
+                errorText = login.errorText
             )
-            Spacer(modifier = Modifier.height(10.dp))
 
             AuthTextField(
-                text = password,
+                text = password.value,
                 label = "Hasło",
                 onValChange = { viewModel.onEvent(AuthEvent.EnteredPassword(it)) },
                 icon = Icons.Rounded.Lock,
-                isPassword = true
+                isPassword = true,
+                isError = password.isError,
+                errorText = password.errorText
             )
-            Spacer(modifier = Modifier.height(20.dp))
 
             Row(
                 modifier = Modifier
@@ -72,11 +103,6 @@ fun LoginScreen(
                 horizontalArrangement = Arrangement.Center
             ) {
                 BetSimButton("Zaloguj"){
-                    navController.navigate(Screen.UserNav.route){
-                        popUpTo(Screen.AuthNav.route){
-                            inclusive = false
-                        }
-                    }
                     viewModel.onEvent(AuthEvent.OnAuthClick)
                 }
             }
@@ -92,7 +118,12 @@ fun LoginScreen(
                 }
             }
         }
+        if (isLoading) {
+            SemiTransparentLoadingScreen()
+        }
     }
+
+
 }
 
 @Preview
--- frontend/app/src/main/java/com/example/betsim/presentation/auth/LoginScreenViewModel.kt
+++ frontend/app/src/main/java/com/example/betsim/presentation/auth/LoginScreenViewModel.kt
@@ -4,6 +4,9 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.betsim.presentation.common.util.TextFieldState
+import com.example.betsim.presentation.common.util.validateTextFieldState
+import com.example.betsim.presentation.common.util.validateTextInput
 import com.example.betsim.repository.BetSimRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -15,34 +18,56 @@ class LoginScreenViewModel @Inject constructor(
     private val repository: BetSimRepository
 ) : ViewModel(){
 
-    private val _login = mutableStateOf("")
-    val login: State<String> = _login
+    private val _login = mutableStateOf(TextFieldState(""))
+    val login: State<TextFieldState<String>> = _login
 
-    private val _password = mutableStateOf("")
-    val password: State<String> = _password
+    private val _password = mutableStateOf(TextFieldState(""))
+    val password: State<TextFieldState<String>> = _password
 
+    private val _toastMessage = mutableStateOf("")
+    val toastMessage: State<String> = _toastMessage
+
+    private val _success = mutableStateOf(false)
+    val success: State<Boolean> =_success
+
+    private val _isLoading = mutableStateOf(false)
+    val isLoading: State<Boolean> = _isLoading
 
 
     fun onEvent(event: AuthEvent){
         when(event){
             is AuthEvent.EnteredLogin -> {
-                _login.value = event.value
+                if (!validateTextInput(event.value)) return
+                _login.value = _login.value.copy(value = event.value)
             }
             is AuthEvent.EnteredPassword -> {
-                _password.value = event.value
+                if (!validateTextInput(event.value)) return
+                _password.value = _password.value.copy(value = event.value)
             }
             AuthEvent.OnAuthClick -> {
-                login()
+                if (!checkInputFields()) return
+                onLogin()
             }
             else -> {}
         }
     }
 
-    private fun login(){
+
+    private fun checkInputFields(): Boolean{
+        _login.value = validateTextFieldState(_login.value)
+        _password.value = validateTextFieldState(_password.value)
+        return !(_login.value.isError || _password.value.isError)
+    }
+
+    private fun onLogin(){
         viewModelScope.launch {
             val login = repository.login("test@test.pl", "testa1")
             val logina = repository.login("test@test.pl", "testa1a")
         }
+    }
+
+    fun clearToast(){
+        _toastMessage.value = ""
     }
 
 }
