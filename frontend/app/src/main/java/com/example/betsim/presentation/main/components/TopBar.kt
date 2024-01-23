2a2d9631fb72435f1ea2e01afac9fbbe1a298513
fibern <domagala.john@gmail.com>
2024-01-23 13:36:59 +0100

    update points onTextClick

--- frontend/app/src/main/java/com/example/betsim/presentation/main/components/TopBar.kt
+++ frontend/app/src/main/java/com/example/betsim/presentation/main/components/TopBar.kt
@@ -3,6 +3,7 @@ package com.example.betsim.presentation.main.components
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -15,6 +16,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
@@ -43,20 +45,28 @@ fun BetSimTopAppBar(
             titleContentColor = MaterialTheme.colorScheme.onPrimary
         ),
         actions = {
-            if (!user.isMod && user.points != null){
-                Text(text = "%.2f".format(user.points).replace('.',','), color = MaterialTheme.colorScheme.onPrimary)
-                Icon(
-                    painterResource(id = ic_casino_chip),
-                    "",
-                    tint = MaterialTheme.colorScheme.onPrimary,
+            if (!user.isMod && user.points != null) {
+                Row(
                     modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .rotate(rotation)
                         .clickable {
                             rotationState = rotation
                             onClick()
-                        }
-                )
+                        },
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "%.2f".format(user.points).replace('.', ','),
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                    Icon(
+                        painterResource(id = ic_casino_chip),
+                        "",
+                        tint = MaterialTheme.colorScheme.onPrimary,
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .rotate(rotation)
+                    )
+                }
             }
         }
     )
